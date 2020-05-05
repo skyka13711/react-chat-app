a5d576c49bebcc143dd1b1be13f721b0e01e6f38
skyka13711 <nikitos13711@gmail.com>
2020-05-05 13:50:47 +0700

    Updates

---
+++ precache-manifest.c4c2cdea37e835e986bab2cd5ff41778.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18efb3d9fff30ed695783b16748baa9",
+    "url": "/react-chat-app/index.html"
+  },
+  {
+    "revision": "112f187be1fdd4e34d19",
+    "url": "/react-chat-app/static/css/2.c8966837.chunk.css"
+  },
+  {
+    "revision": "7918a95be66f68528586",
+    "url": "/react-chat-app/static/css/main.7b3132bd.chunk.css"
+  },
+  {
+    "revision": "112f187be1fdd4e34d19",
+    "url": "/react-chat-app/static/js/2.23dbe860.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/react-chat-app/static/js/2.23dbe860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7918a95be66f68528586",
+    "url": "/react-chat-app/static/js/main.78b5c3d4.chunk.js"
+  },
+  {
+    "revision": "40280657cfe86b036778",
+    "url": "/react-chat-app/static/js/runtime-main.96232b0b.js"
+  },
+  {
+    "revision": "c1f419d6abb1c32f97508b61040b2074",
+    "url": "/react-chat-app/static/media/checked.c1f419d6.svg"
+  },
+  {
+    "revision": "1456f01338efe100733984d48807dd08",
+    "url": "/react-chat-app/static/media/noChecked.1456f013.svg"
+  },
+  {
+    "revision": "7c6628a29a3fa0dff54199c29805f3df",
+    "url": "/react-chat-app/static/media/test.7c6628a2.png"
+  }
+]);
